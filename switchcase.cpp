ad61f1558a3df82e7f147dd66673bc623f3697c7
devwithlogic <devsingh.ds2803@gmail.com>
2026-07-19 22:44:27 +0530

    add switch case code

---
+++ switchcase.cpp
@@ -0,0 +1,39 @@
+/*Given the integer day denoting the day number,
+ print on the screen which day of the week it is. 
+Week starts from Monday and for values greater than 7 or less than 1, print Invalid.*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day;
+    cout << "Enter the day number: ";
+    cin >> day;
+
+    switch (day) {
+        case 1:
+            cout << "Monday" << endl;
+            break;
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Invalid" << endl;
+    }
+
+    return 0;
+}
